96b0721d4bc2ebae68cfe76f292dd77ab5fc4203
Sheraz Hussain <162985175+sherazcs23@users.noreply.github.com>
2024-11-25 22:36:48 +0500

    Add files via upload

---
+++ Fundamentals part2/script.js
@@ -0,0 +1,62 @@
+ use = 'strict';
+// declaring a function name sayhi
+// name , age in the  parentheses is the parameter
+
+// function sayHi(name,age) {     
+//     return `Hi ${name}!, you are ${age} years  old now !`;
+// }
+ 
+//  // callig the function // output: Hi sheraz  you are 21 years old !
+//  console.log(sayHi('亗S H E R A Z彡',21));
+ 
+//  Fuction declaration 
+//    function addition(num1,num2) {
+
+//         return "The sum of " + num1 + " and " + num2 + " is " + "=" + " " + (num1 + num2);   // ths is string concatenation concept
+//     }
+     
+//    console.log(addition(5,9));  // output: The sum of 5 and 9 is 14
+
+//    function subtraction(h4,h5) {
+       
+//     return "The Difference of " + h4 + " and " + h5 + " is " + "="  + " " + (h4 - h5);   // ths is string concatenation concept
+// }
+//  const result = subtraction(54,6);
+// console.log(result); 
+
+// function multiplication(s,t) {
+     
+//     return "The Product of " + s + " and " + t + " is " + "=" + " " + (s * t);   // ths is string concatenation concept
+// }
+//  const  nawad = multiplication(8,9);
+// console.log(nawad); 
+
+  //  function declaration
+// function divison(x,y) {
+        
+//     return "The divison of " + x + " and " + y + " is " + "=" + " " + (x / y);   // ths is string concatenation concept
+// }
+//     const ans = divison(5,6);
+// console.log(ans); 
+
+// // function expresion 
+
+// const division = function(x,y) {
+
+//    result = `the division of ${x} and ${y} is = ${x/y}`;
+//     return result;
+// }
+
+//  const answer = division (7,8);
+//     console.log(answer);
+
+// function expression examples 
+
+//     const calculate = function(currentage) {
+//         const result  = 2024 - currentage;
+//         return `your age at now is approximately ${result}`;
+//     }
+
+//     const answer = calculate(2003);
+//    console.log(answer);
+
